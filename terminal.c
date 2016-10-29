d0cf42d3688090b46e2be9edfbfc7f2db641b383
arvikachristy <anastasia.arvika@gmail.com>
2016-10-29 13:50:16 +0100

    Push code remote :jack_o_lantern:

---
+++ terminal.c
@@ -0,0 +1,146 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <dirent.h>
+#define MAXINT 1024
+
+char *home;
+char *path;
+char *arrayPath[256];
+char *arrayInput[256];
+
+char *getDirectory(){
+   static char cwd[1024];
+   if (getcwd(cwd, sizeof(cwd)) != NULL)
+       return cwd;
+   else
+       perror("getcwd() error");
+   return 0;
+}
+
+bool stringStart(const char *a,const char *b){
+  if(strncmp(a, b, strlen(b)) == 0){
+	return 1;
+  }
+  return 0;
+}
+
+int colonSplitter(char* buf, char* x){
+    int i = 0;
+    char *p = strtok (buf, x);
+
+    while (p != NULL)
+    {
+        arrayPath[i++] = p;
+        p = strtok (NULL, x);
+    }
+
+    for (i = 0; i < 3; ++i) 
+        printf("%s\n", arrayPath[i]);
+    //strcpy(arrayPath, array);
+
+    return 0;    
+}
+
+int spaceSplitter(char* buf){
+    int i = 0;
+    char *p = strtok (buf, " ");
+
+    while (p != NULL)
+    {
+        arrayInput[i++] = p;
+        p = strtok (NULL, " ");
+    }
+    return 0;    
+}
+
+bool openDirectory2(char* inputFile){
+//this function is to open the directory if it exists
+    DIR* dir = opendir("mydir");
+    int u;
+    for(u=0;u<sizeof(arrayInput);u++){
+	if(arrayInput[u]==NULL){
+	    break;
+	}else{
+	    if (dir){
+	    /* Directory exists. */
+	     return true;
+	     closedir(dir);
+	    }else if (ENOENT == errno){
+	    /* Directory does not exist. */
+	     return false;
+	    }else{
+	     return false;/* opendir() failed for some other reason. */
+	    }	  
+	}
+    }
+}
+
+int openDirectory(char *argc)
+{
+    const char* folderr;
+    //folderr = "C:\\Users\\SaMaN\\Desktop\\Ppln";
+    folderr = "/bin";
+    struct stat sb;
+
+    if (stat(folderr, &sb) == 0 && S_ISDIR(sb.st_mode))
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+}
+
+
+
+int getFile(){
+//reading the file from profile   
+    FILE* file = fopen("profile", "r");
+    char line[256];
+    char substring[256];
+
+    while (fgets(line, sizeof(line), file)) {
+	if(stringStart(line, "HOME")){
+	  strcpy(home, line);
+          //printf("%s", home); //GET RID DEBUGGING
+	}else if(stringStart(line, "PATH")){
+	  strncpy(substring, line+5, sizeof(line));
+	  strcpy(path, substring);
+          printf("%s", colonSplitter(path, ":")); //GET RID  DEBUGGING
+	}
+    }
+    if (ferror(file)) {
+	/* deal with error */
+    }
+
+    fclose(file);
+
+    return 0;
+}
+
+int main() {
+    home  = malloc(256);
+    path  = malloc(256);
+    getFile();
+
+    while(1){
+        char data[200];
+    	printf("%s> ", getDirectory());
+	fgets(data, 300, stdin);
+	spaceSplitter(data);//split the input's white space
+
+	if(openDirectory(arrayInput[0])){
+          printf("%s\n", arrayInput[0]);
+	}
+    }
+
+    free(home);
+    free(path);
+}
+	
